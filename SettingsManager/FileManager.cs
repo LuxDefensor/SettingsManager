62576710d72e7aed797cff8176ad24e0afa6e0d2
Reset <lux_defensor@mail.ru>
2017-12-19 08:08:20 +0300

    stable version

---
+++ SettingsManager/FileManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Settings
+{
+    internal class FileManager
+    {
+        private string fileName;
+
+        public FileManager() : this("settings.ini")
+        {
+
+        }
+        public FileManager(string fileName)
+        {
+            this.fileName = fileName;
+            if (!File.Exists(fileName))
+            {
+                FileStream f = File.Create(fileName);
+                f.Close();
+            }
+        }
+
+        public void WriteAll(string[] newSettings)
+        {
+            string[] existing = File.ReadAllLines(fileName);
+            foreach (string s in newSettings)
+            {
+                bool found = false;
+                for (int i = 0; i < existing.Length; i++)
+                {
+                    if (existing[i].Substring(0, existing[i].IndexOf('=')).Trim() ==
+                        s.Substring(0, s.IndexOf('=')).Trim())
+                    {
+                        existing[i] = s;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    string[] newArray = new string[existing.Length + 1];
+                    existing.CopyTo(newArray, 0);
+                    newArray[existing.Length] = s;
+                    existing = newArray;
+                }
+            }
+            File.WriteAllLines(fileName, existing);
+        }
+
+        public void WriteAll(Dictionary<string, string> newSettings)
+        {
+            WriteAll(newSettings.Select(kvp => kvp.Key + "=" + kvp.Value).ToArray());
+        }
+
+        public string[] ReadAll()
+        {
+            return File.ReadAllLines(fileName);
+        }
+    }
+}
---
+++ SettingsManager/SettingsManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Settings
+{
+    public class SettingsManager
+    {
+        private FileManager f;
+        private Dictionary<string, string> settings;
+
+        public SettingsManager(string fileName, string[] defaultSettings)
+            : this(fileName)
+        {
+            f.WriteAll(defaultSettings);
+            if (defaultSettings.Length > 0)
+                f.WriteAll(defaultSettings);
+        }
+
+        public SettingsManager(string fileName, Dictionary<string, string> defaultSettings)
+            : this(fileName, defaultSettings.Select(kvp => kvp.Key + "=" + kvp.Value).ToArray())
+        {
+        }
+
+        public SettingsManager(string fileName)
+        {
+            f = new FileManager(fileName);
+            settings = f.ReadAll().ToDictionary<string, string, string>(s => s.Substring(0, s.IndexOf('=')).Trim(),
+                                         s => s.Substring(s.IndexOf('=') + 1).Trim());
+        }
+
+        public SettingsManager() : this("settings.ini")
+        {
+
+        }
+
+        public string this[string i]
+        {
+            get
+            {
+                return settings[i];
+            }
+            set
+            {
+                if (settings.ContainsKey(i))
+                    settings[i] = value;
+                else
+                    settings.Add(i, value);
+                f.WriteAll(settings);
+            }
+        }
+
+        public void ChangeSettings(Dictionary<string, string> newSettings)
+        {
+            foreach (var kvp in newSettings)
+            {
+                if (settings.ContainsKey(kvp.Key))
+                    settings[kvp.Key] = kvp.Value;
+                else
+                    settings.Add(kvp.Key, kvp.Value);
+            }
+            f.WriteAll(settings);
+        }
+
+        public void ChangeSettings(string[] newSettings)
+        {
+            ChangeSettings(newSettings.ToDictionary<string, string, string>(s => s.Substring(0, s.IndexOf('=')).Trim(),
+                                                         s => s.Substring(s.IndexOf('=') + 1).Trim()));
+        }
+
+        public bool HasKey(string key)
+        {
+            return settings.ContainsKey(key);
+        }
+                
+    }
+}
